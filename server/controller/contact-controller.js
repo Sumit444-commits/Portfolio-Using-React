bebb2716970438fb99ed97e7ebaa526e8e175fdf
Sumit <121432275+Sumit444-commits@users.noreply.github.com>
2025-08-31 11:32:46 +0500

    Log request body in contactUs controller

--- server/controller/contact-controller.js
+++ server/controller/contact-controller.js
@@ -3,6 +3,7 @@ import { sendEmail } from "../middleware/email-middleware.js";
 const contactUs = async (req, res) => {
   try {
     const { name, email, subject, message } = req.body;
+     console.log(res.body)
     sendEmail(name, email, subject, message);
      res.status(200).json({
         message: "Contact sent",
